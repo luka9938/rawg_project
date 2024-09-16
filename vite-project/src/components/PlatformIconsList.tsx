c7e0ff14d8b83789619f8f7c997763efbca85610
Lukas G <luka9938@stud.kea.dk>
2024-09-16 09:48:17 +0200

    icons

--- vite-project/src/components/PlatformIconsList.tsx
+++ vite-project/src/components/PlatformIconsList.tsx
@@ -1,15 +1,41 @@
-import { Text } from "@chakra-ui/react";
+import { Icon } from "@chakra-ui/react";
 import { Platform } from "../hooks/useGames";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
 
 interface Props {
   platforms: Platform[];
 }
 
 const PlatformIconsList = ({ platforms }: Props) => {
+  const getIcon = (slug: string) => {
+    switch (slug) {
+      case "windows":
+        return FaWindows;
+      case "playstation":
+        return FaPlaystation;
+      case "xbox":
+        return FaXbox;
+      case "mac":
+        return FaApple;
+      case "linux":
+        return FaLinux;
+      case "android":
+        return FaAndroid;
+      default:
+        return FaWindows;
+    }
+  };
   return (
     <>
       {platforms.map((platform) => (
-        <Text key={platform.id}>{platform.name}</Text>
+        <Icon key={platform.id} as={getIcon(platform.slug)} />
       ))}
     </>
   );

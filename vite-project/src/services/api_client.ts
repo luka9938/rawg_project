0825812268f1155243d857ebd499b60ede4e5ac6
Lukas G <luka9938@stud.kea.dk>
2024-10-11 10:35:43 +0200

    fixes

--- vite-project/src/services/api_client.ts
+++ vite-project/src/services/api_client.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://api.rawg.io/api",
+  // baseURL: "https://api.rawg.io/api",
+  baseURL: "http://localhost:5000/",
+
   params: {
     key: import.meta.env.VITE_API_KEY,
   },

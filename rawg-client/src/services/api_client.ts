35f92e511c819fdf2a12231fca95b4165110fe90
Lukas G <luka9938@stud.kea.dk>
2024-10-21 09:58:17 +0200

    shit works

--- rawg-client/src/services/api_client.ts
+++ rawg-client/src/services/api_client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   // baseURL: "https://api.rawg.io/api",
-  baseURL: "http://localhost:5000/",
+  baseURL: "http://localhost:5001/",
 
   params: {
     key: import.meta.env.VITE_API_KEY,

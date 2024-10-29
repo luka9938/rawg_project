3bcd6a36261150c36fbb659648ab181f0ea42559
Lukas G <luka9938@stud.kea.dk>
2024-10-29 17:38:02 +0100

    fix

--- rawg-client/src/services/api_client.ts
+++ rawg-client/src/services/api_client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   // baseURL: "https://api.rawg.io/api",
-  baseURL: "http://localhost:5001/",
+  baseURL: import.meta.env.VITE_API_URL,
 
   params: {
     key: import.meta.env.VITE_API_KEY,
